f5f1d9069a7d0a68942555e23c733e946f5f7483
timm.landes <timm.landes@hot.uni-hannover.de>
2025-10-22 20:17:49 +0200

    DHO fitting improved to accomodate 3d (xyz) scans

--- BrillouinAnalyzer/analysis/fitmodel.py
+++ BrillouinAnalyzer/analysis/fitmodel.py
@@ -31,7 +31,7 @@ class DHO(FitStep):
 
     """
     def __init__(self, *, expected_peaks, p0, bounds):
-        super().__init__(_fit_concurrent, expected_peaks=expected_peaks, p0=p0, bounds=bounds)
+        super().__init__(_fit_concurrent_DHO2, expected_peaks=expected_peaks, p0=p0, bounds=bounds)
 
     
 class Lorentzian(FitStep):
@@ -56,20 +56,20 @@ class Lorentzian(FitStep):
         super().__init__(_fit_concurrent_lorentzian, expected_peaks=expected_peaks, p0=p0, bounds=bounds)
 
 
-def _fitDHO(x, y, X, intensity_data_slice, expected_peaks, p0, bounds, fit_funcs):
+def _fitDHO(x, y, z, t, spectral_axis, intensity_data_slice, expected_peaks, p0, bounds, fit_funcs):
     # Convert masked values to nan
     if np.ma.is_masked(intensity_data_slice):
         # Check if all values are masked
         if np.ma.getmask(intensity_data_slice).all():
-            return x, y, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
-        Y = intensity_data_slice.filled(np.nan)
+            return x, y, z, t, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
+        intensity_axis = intensity_data_slice.filled(np.nan)
     else:
-        Y = intensity_data_slice.copy()
-    Y[Y == 0] = np.nan
+        intensity_axis = intensity_data_slice.copy()
+    intensity_axis[intensity_axis == 0] = np.nan
     
     # If all values are NaN, return NaN results
-    if np.all(np.isnan(Y)):
-        return x, y, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
+    if np.all(np.isnan(intensity_axis)):
+        return x, y, z, t, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
     
     if p0 is None:
         p0 = [1] * (expected_peaks * 3 + 2)  # Default initial guesses
@@ -78,12 +78,60 @@ def _fitDHO(x, y, X, intensity_data_slice, expected_peaks, p0, bounds, fit_funcs
 
     try:
         # Use nan_policy='omit' to handle NaN values
-        popt, pcov = curve_fit(fit_funcs[expected_peaks], X, Y, p0=p0, bounds=bounds, nan_policy='omit')
-        return x, y, popt, pcov
+        popt, pcov = curve_fit(fit_funcs[expected_peaks], spectral_axis, intensity_axis, p0=p0, bounds=bounds, nan_policy='omit')
+        return x, y, z, t, popt, pcov
     except RuntimeError:
-        print(f"Fit not successful for pixel ({x},{y})")
-        return x, y, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
+        print(f"Fit not successful for pixel ({x},{y},{z},{t})")
+        return x, y, z, t, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
 
+def _fitDHO2(idx, spectral_axis, intensity_data_slice, expected_peaks, p0, bounds, fit_func):
+    # Convert masked values to nan
+    if np.ma.is_masked(intensity_data_slice):
+        if np.ma.getmask(intensity_data_slice).all():
+            return idx, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
+        intensity_axis = intensity_data_slice.filled(np.nan)
+    else:
+        intensity_axis = intensity_data_slice.copy()
+    intensity_axis[intensity_axis == 0] = np.nan
+
+    if np.all(np.isnan(intensity_axis)):
+        return idx, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
+
+    if p0 is None:
+        p0 = [1] * (expected_peaks * 3 + 2)
+    if bounds is None:
+        bounds = (0, np.inf)
+
+    try:
+        popt, pcov = curve_fit(fit_func, spectral_axis, intensity_axis, p0=p0, bounds=bounds, nan_policy='omit')
+        return idx, popt, pcov
+    except RuntimeError:
+        print(f"Fit not successful for pixel {idx}")
+        return idx, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
+    
+def _fit_concurrent_DHO2(intensity_data, spectral_axis, expected_peaks, p0=None, bounds=None):
+    variables = int(expected_peaks * 3 + 2)
+    spatial_shape = intensity_data.shape[:-1]
+    fit_results = np.zeros(spatial_shape + (variables,))
+    cov_results = np.empty(spatial_shape + (variables, variables))
+
+    fit_funcs = {1: _DHO_1, 2: _DHO_2, 3: _DHO_3}
+    fit_func = fit_funcs[expected_peaks]
+    spectral_axis = spectral_axis[0]
+    print('Starting of Multiprocessing can take up to 10 seconds.')
+    tasks = []
+    with ProcessPoolExecutor(max_workers=max(1,int(os.cpu_count()*0.75))) as executor:
+        for idx in np.ndindex(spatial_shape):
+            intensity_data_slice = intensity_data[idx + (slice(None),)]
+            task = executor.submit(_fitDHO2, idx, spectral_axis, intensity_data_slice, expected_peaks, p0, bounds, fit_func)
+            tasks.append(task)
+
+        for future in tqdm(as_completed(tasks), total=len(tasks), desc='Fitting Spectral data'):
+            idx, popt, pcov = future.result()
+            fit_results[idx] = popt
+            cov_results[idx] = pcov
+            
+    return fit_results, cov_results
 
 def _fitLorentzian(x, y, X, intensity_data_slice, expected_peaks, p0, bounds, fit_funcs):
     # Convert masked values to nan
@@ -107,35 +155,37 @@ def _fitLorentzian(x, y, X, intensity_data_slice, expected_peaks, p0, bounds, fi
         return x, y, np.full(expected_peaks * 3 + 2, np.nan), np.full((expected_peaks * 3 + 2, expected_peaks * 3 + 2), np.nan)
 
 
-def _fit_concurrent(intensity_data, spectral_axis, expected_peaks, p0=None, bounds=None):
+def _fit_concurrent_DHO(intensity_data, spectral_axis, expected_peaks, p0=None, bounds=None):
     variables = int(expected_peaks * 3 + 2)
     if p0 is not None and len(p0) != variables:
         p0 = None
         print('Length of p0 does not match the model. Fallback to p0 = None')
-    fit_results = np.zeros((intensity_data.shape[0], intensity_data.shape[1], variables))
-    cov_results = np.empty((intensity_data.shape[0], intensity_data.shape[1], variables, variables))
+    fit_results = np.zeros((intensity_data.shape[0], intensity_data.shape[1], intensity_data.shape[2], intensity_data.shape[3], variables))
+    cov_results = np.empty((intensity_data.shape[0], intensity_data.shape[1], intensity_data.shape[2], intensity_data.shape[3], variables, variables))
 
     fit_funcs = {1: _DHO_1, 2: _DHO_2, 3: _DHO_3}
-    X = spectral_axis[0]
+    spectral_axis = spectral_axis[0]
     print('Starting of Multiprocessing can take up to 10 seconds.')
     tasks = []
     with ProcessPoolExecutor(max_workers=max(1,int(os.cpu_count()*0.75))) as executor:
         for x in range(intensity_data.shape[0]):
             for y in range(intensity_data.shape[1]):
-                intensity_data_slice = intensity_data[x, y, :]
-                # Skip if all values are masked
-                if np.ma.is_masked(intensity_data_slice) and np.ma.getmask(intensity_data_slice).all():
-                    fit_results[x, y, :] = np.nan
-                    cov_results[x, y, :, :] = np.nan
-                    continue
-                task = executor.submit(_fitDHO, x, y, X, intensity_data_slice, expected_peaks, p0, bounds, fit_funcs)
-                tasks.append(task)
+                for z in range(intensity_data.shape[2]):
+                    for t in range(intensity_data.shape[3]):
+                            intensity_data_slice = intensity_data[x, y, z, t, :]
+                            # Skip if all values are masked
+                            # if np.ma.is_masked(intensity_data_slice) and np.ma.getmask(intensity_data_slice).all():
+                            #     fit_results[x, y, z, t, :] = np.nan
+                            #     cov_results[x, y, z, t, :] = np.nan
+                            #     continue
+                            task = executor.submit(_fitDHO, x, y, z, t,  spectral_axis, intensity_data_slice, expected_peaks, p0, bounds, fit_funcs)
+                            tasks.append(task)
         
         # Use tqdm to display a progress bar
         for future in tqdm(as_completed(tasks), total=len(tasks), desc='Fitting Spectral data'):
-            x, y, popt, pcov = future.result()
-            fit_results[x, y, :] = popt
-            cov_results[x, y, :, :] = pcov
+            x, y, z, t, popt, pcov = future.result()
+            fit_results[x, y, z, t, :] = popt
+            cov_results[x, y, z, t, :, :] = pcov
 
     return fit_results, cov_results
 
@@ -170,6 +220,7 @@ def _fit_concurrent_lorentzian(intensity_data, spectral_axis, expected_peaks, p0
             cov_results[x, y, :, :] = pcov
 
     return fit_results, cov_results
+
 
 def _DHO_1(x, I0, freqShift, LineWidth, Background, Asymmetry):
     return I0 * 4 * LineWidth * freqShift**2 /(np.pi*(((x-Asymmetry)**2 - freqShift**2)**2 + 4*(LineWidth*(x-Asymmetry))**2)) + Background
